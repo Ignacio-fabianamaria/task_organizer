5c4e8a31e52b29118f103b4f82c303a2dcdee7d9
Siigmaf <faby.tbe@gmail.com>
2023-08-08 17:44:30 -0300

    feat: add input and button to save the tasks

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,10 +1,18 @@
 import { Header } from "../../components/Header";
-// import styles from './Home.module.css'
+import styles from './Home.module.css'
 
 export function Home(){
     return(
         <div>
             <Header />
+            <div className={styles.createTask}>
+                <input type="text" />
+                <button>Adicionar</button>
+            </div>
+            <div className={styles.filter}>
+                <span className={styles.finish}>Finalizados: 5 tarefas</span>
+                <span className={styles.progress}>Em progresso: 5 tarefas</span>
+            </div>
         </div>
     )
 }
